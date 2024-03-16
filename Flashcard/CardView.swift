4e4809ea0ac3308e5934aa19fd9ddfcf80faf5d3
Ryo Fujimura <ryo176999@gmail.com>
2024-03-16 09:43:25 -0700

    card view + swipe gesture

---
+++ Flashcard/CardView.swift
@@ -0,0 +1,100 @@
+//
+//  CardView.swift
+//  Flashcard
+//
+//  Created by ryo fujimura on 3/16/24.
+//
+
+import SwiftUI
+
+// Card data model
+struct Card {
+    let question: String
+    let answer: String
+    
+    static let mockedCards = [
+            Card(question: "Located at the southern end of Puget Sound, what is the capitol of Washington?", answer: "Olympia"),
+            Card(question: "Which city serves as the capital of Texas?", answer: "Austin"),
+            Card(question: "What is the capital of New York?", answer: "Albany"),
+            Card(question: "Which city is the capital of Florida?", answer: "Tallahassee"),
+            Card(question: "What is the capital of Colorado?", answer: "Denver")
+        ]
+}
+
+struct CardView: View {
+    let card: Card
+    private let swipeThreshold: Double = 200 // <--- Define a swipeThreshold constant top level
+    @State private var isShowingQuestion = true
+    @State private var offset: CGSize = .zero
+    var onSwipedLeft: (() -> Void)? // <-- Add closures to be called when user swipes left or right
+    var onSwipedRight: (() -> Void)? // <--
+    
+    var body: some View {
+        ZStack {
+            // Back-most card background
+            RoundedRectangle(cornerRadius: 25.0) // <-- Add another card background behind the original
+                .fill(offset.width < 0 ? .red : .green) // <-- Set fill based on offset (swipe left vs right)
+
+            // Front-most card background (i.e. original background)
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(isShowingQuestion ? Color.blue.gradient : Color.indigo.gradient)
+                .shadow(color: .black, radius: 4, x: -2, y: 2)
+                .opacity(1 - abs(offset.width) / swipeThreshold) // <-- Fade out front-most background as user swipes
+            VStack(spacing: 20) {
+
+                // Card type (question vs answer)
+                Text(isShowingQuestion ? "Question" : "Answer")
+                    .bold()
+
+                // Separator
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(isShowingQuestion ? .blue : .indigo)
+
+                // Card text
+                Text(isShowingQuestion ? card.question : card.answer)
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding()
+        }
+        .frame(width: 300, height: 500)
+        .onTapGesture {
+            isShowingQuestion.toggle()
+        }
+        .offset(CGSize(width: offset.width, height: 0))
+        .rotationEffect(.degrees(offset.width / 20.0)) // <-- Add rotation when swiping
+        .offset(CGSize(width: offset.width, height: 0))
+        .opacity(3 - abs(offset.width) / swipeThreshold * 3) // <-- Fade the card out as user swipes, beginning fade in the last 1/3 to the threshold
+        .rotationEffect(.degrees(offset.width / 20.0))
+        .offset(CGSize(width: offset.width, height: 0))
+        .gesture(DragGesture()
+            .onChanged { gesture in
+                let translation = gesture.translation
+                 print(translation)
+                offset = translation
+            }.onEnded { gesture in  // <-- onEnded called when gesture ends
+
+                if gesture.translation.width > swipeThreshold {
+                    print("👉 Swiped right")
+                    onSwipedRight?() // <-- Call swiped right closure
+                } else if gesture.translation.width < -swipeThreshold {
+                    print("👈 Swiped left")
+                    onSwipedLeft?() // <-- Call swiped left closure
+                } else {
+                    print("🚫 Swipe canceled")
+                    withAnimation(.bouncy) { // <-- Make updates to state managed property with animation
+                        offset = .zero
+                    }
+                }
+            }
+        )
+    }
+}
+
+
+
+#Preview {
+    CardView(card: Card(
+        question: "Located at the western end of Puget Sound, what is the capitol of Washington?",
+        answer: "Olympia"))
+}
--- Flashcard/ContentView.swift
+++ Flashcard/ContentView.swift
@@ -6,81 +6,28 @@
 //
 
 import SwiftUI
-import CoreData
 
+// Define the ContentView structure, which conforms to the View protocol
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var cards: [Card] = Card.mockedCards
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        ZStack {
+            ForEach(0..<cards.count, id: \.self) { index in
+                CardView(card: cards[index])
+                    .rotationEffect(.degrees(Double(cards.count - 2 - index) * -3))
             }
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        .gesture(DragGesture()
+            .onChanged { gesture in // <-- onChanged called for every gesture value change, like when the drag translation changes
+                let translation = gesture.translation // <-- Get the current translation value of the gesture. (CGSize with width and height)
+                print(translation) // <-- Print the translation value
             }
-        }
+        )
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
+// Preview the ContentView in the canvas
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
 }
